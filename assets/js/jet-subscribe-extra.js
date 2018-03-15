caf6cf5967f8989b790616a6fd8a738e65134745
shinTM <emasugeta@gmail.com>
2018-03-15 14:35:06 +0200

    wip

--- assets/js/jet-subscribe-extra.js
+++ assets/js/jet-subscribe-extra.js
@@ -6,10 +6,13 @@
 
 		init: function() {
 			$( window ).on( 'jet-elements/subscribe', function( event ) {
-				var elementId = event.elementId,
-					$popup    = $( '.jet-subscribe-extra-popup' );
+				var elementId   = event.elementId,
+					successType = event.successType,
+					$popup      = $( '.jet-subscribe-extra-popup' );
 
-				$popup.addClass( 'opened-state' );
+				if ( 'success' === successType ) {
+					$popup.addClass( 'opened-state' );
+				}
 			});
 
 			$( document ).on( 'click.JetSubscribeExtra', '.jet-subscribe-extra-popup__close, .jet-subscribe-extra-popup__cover', function( event ) {
